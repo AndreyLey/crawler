c0a4d5021e2b60854991cf7798d7fc7e5635dd2c
AndreyLey <andreyleykin@gmail.com>
2020-10-26 20:25:40 +0200

    added options for mongo connection

--- BackEndCrawler/server.js
+++ BackEndCrawler/server.js
@@ -12,11 +12,11 @@ const PROTOCOL = 'http';
 const DOMAIN = process.env.DOMAIN || 'localhost';
 const PORT = process.env.PORT || 5050;
 
-const MONGO_PORT = process.env.MONGO_PORT || 27018;
+const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 const MONGO_DB = process.env.MONGO_HOST || 'crawler';
 
-Mongo_Client.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}`, (err, client)=>{
+Mongo_Client.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}`, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client)=>{
     if(err) return console.log(err);
     const db =client.db(MONGO_DB);
     app.db= db;
